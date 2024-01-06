c843d12b7685bd374a2444a2a0a8d3fc1cd79985
daviszhen <daviszhen007@gmail.com>
2024-01-06 15:54:45 +0800

    update pager

--- exec.go
+++ exec.go
@@ -4,12 +4,12 @@ type Vector interface {
 }
 
 type Chunk interface {
-    Count() int
-    GetVector(int) Vector
+	Count() int
+	GetVector(int) Vector
 }
 
 type Exec interface {
-    Open() error
-    GetNext() (Chunk, error)
-    Close() error
+	Open() error
+	GetNext() (Chunk, error)
+	Close() error
 }
--- util_test.go
+++ util_test.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "github.com/xlab/treeprint"
-    "testing"
+	"fmt"
+	"github.com/xlab/treeprint"
+	"testing"
 )
 
 func TestTreePrint(t *testing.T) {
-    tree := treeprint.New()
-    tree.AddNode("root")
-    proj := tree.AddMetaBranch("abc", "Projects:")
-    proj.AddNode("n1")
-    sub := proj.AddBranch("n2")
-    sub.AddNode("n3")
-    sub.AddNode("n4")
-    tree.AddMetaBranch("def", "Projects2:")
+	tree := treeprint.New()
+	tree.AddNode("root")
+	proj := tree.AddMetaBranch("abc", "Projects:")
+	proj.AddNode("n1")
+	sub := proj.AddBranch("n2")
+	sub.AddNode("n3")
+	sub.AddNode("n4")
+	tree.AddMetaBranch("def", "Projects2:")
 
-    fmt.Println(tree.String())
+	fmt.Println(tree.String())
 }
